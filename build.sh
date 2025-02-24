e39d2affff4251a07a0c50c073f17f3494db526b
KickedDroid <24928676+KickedDroid@users.noreply.github.com>
2025-02-24 17:55:33 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#cargo build --lib --target x86_64-pc-windows-gnu --release
-# For sliver extensions
-#cp /home/zdroid/Documents/Pentesting/sliver-oxide/target/x86_64-pc-windows-gnu/release/sliver_oxide.dll .
-# For Sliver Bof Development
-#x86_64-w64-mingw32-objcopy -O pe-x86-64 sliver_oxide.dll sliver_oxide.o
-
-
-#rustc --target x86_64-pc-windows-gnu -C opt-level=z -C panic=abort -C link-args=-Wl,--subsystem,windows --crate-type=staticlib src/lib.rs -o sliver_oxide.o
-
-
 
 
 RUSTFLAGS="-C target-cpu=x86-64 -C target-feature=+crt-static -C link-arg=-nostartfiles -C link-arg=-nodefaultlibs -C link-arg=-Wl,--gc-sections" \
