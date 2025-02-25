88cabb3805c2a8c237601b5b8bf7a0f99c45f4b3
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2025-02-25 11:29:42 -0600

    .gitignore

--- src/beacon.rs
+++ src/beacon.rs
@@ -7,11 +7,51 @@ pub type BeaconFormatAllocFn = unsafe extern "C" fn(*mut FormatP, c_int);
 pub type BeaconFormatFreeFn = unsafe extern "C" fn(*mut FormatP);
 pub type BeaconPrintfFn = unsafe extern "C" fn(c_int, *const c_char, ...);
 
+type HANDLE = *mut c_void;
+type BOOL = c_int;
+type DWORD = c_uint;
+type LPVOID = *mut c_void;
+type LPCVOID = *const c_void;
+type SIZE_T = usize;
+type LPHANDLE = *mut HANDLE;
+type PDWORD = *mut DWORD;
+
+// Memory-related functions
+pub struct BeaconMemoryFunctions {
+    pub virtual_alloc: unsafe extern "C" fn(LPVOID, SIZE_T, DWORD, DWORD) -> LPVOID,
+    pub virtual_alloc_ex: unsafe extern "C" fn(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) -> LPVOID,
+    pub virtual_protect: unsafe extern "C" fn(LPVOID, SIZE_T, DWORD, PDWORD) -> BOOL,
+    pub virtual_protect_ex: unsafe extern "C" fn(HANDLE, LPVOID, SIZE_T, DWORD, PDWORD) -> BOOL,
+    pub virtual_free: unsafe extern "C" fn(LPVOID, SIZE_T, DWORD) -> BOOL,
+    pub virtual_query:
+        unsafe extern "C" fn(LPCVOID, *mut MEMORY_BASIC_INFORMATION, SIZE_T) -> SIZE_T,
+    pub unmap_view_of_file: unsafe extern "C" fn(LPCVOID) -> BOOL,
+}
+
+// Process and thread functions
+pub struct BeaconProcessFunctions {
+    pub open_process: unsafe extern "C" fn(DWORD, BOOL, DWORD) -> HANDLE,
+    pub open_thread: unsafe extern "C" fn(DWORD, BOOL, DWORD) -> HANDLE,
+    pub close_handle: unsafe extern "C" fn(HANDLE) -> BOOL,
+    pub get_thread_context: unsafe extern "C" fn(HANDLE, *mut CONTEXT) -> BOOL,
+    pub set_thread_context: unsafe extern "C" fn(HANDLE, *const CONTEXT) -> BOOL,
+    pub resume_thread: unsafe extern "C" fn(HANDLE) -> DWORD,
+    pub duplicate_handle:
+        unsafe extern "C" fn(HANDLE, HANDLE, HANDLE, LPHANDLE, DWORD, BOOL, DWORD) -> BOOL,
+    pub read_process_memory:
+        unsafe extern "C" fn(HANDLE, LPCVOID, LPVOID, SIZE_T, *mut SIZE_T) -> BOOL,
+    pub write_process_memory:
+        unsafe extern "C" fn(HANDLE, LPVOID, LPCVOID, SIZE_T, *mut SIZE_T) -> BOOL,
+}
+
 pub struct Beacon {
-    pub(crate) output: BeaconOutputFn,
-    pub(crate) format_alloc: BeaconFormatAllocFn,
-    pub(crate) format_free: BeaconFormatFreeFn,
-    pub(crate) printf: BeaconPrintfFn,
+    output: BeaconOutputFn,
+    format_alloc: BeaconFormatAllocFn,
+    format_free: BeaconFormatFreeFn,
+    printf: BeaconPrintfFn,
+    buffer: FormatP,
+    pub memory: BeaconMemoryFunctions,
+    pub process: BeaconProcessFunctions,
 }
 
 #[repr(C)]
@@ -83,4 +123,3 @@ impl Drop for Beacon {
         self.free();
     }
 }
-
